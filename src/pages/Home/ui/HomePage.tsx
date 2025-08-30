be68f532051af543ba804ab306d1c6416850a651
Aswin Asok <aswinasokofficial@gmail.com>
2025-08-30 11:00:36 +0530

    feat(theming):color-changes-working

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,8 +1,19 @@
+import { useTheme } from "@/shared/config";
 import { Link } from "react-router";
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
+    const { toggleTheme } = useTheme();
     return (
         <>
+            <h1 className={styles.title}>Home Page</h1>
+            <button
+                onClick={() => {
+                    toggleTheme();
+                }}
+            >
+                Change Theme
+            </button>
             <Link to="/login">Go to Login Page</Link>
         </>
     );
--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,4 @@
 import { Theme, ThemeContext, type ThemeType, LOCAL_STORAGE_THEME_KEY } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
 
-export { Theme, ThemeContext, type ThemeType, LOCAL_STORAGE_THEME_KEY };
+export { Theme, ThemeContext, type ThemeType, LOCAL_STORAGE_THEME_KEY, useTheme };
