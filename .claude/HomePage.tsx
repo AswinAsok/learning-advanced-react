d3a9a54c30d8284e7dac269ecfd5c6cc17db8914
Aswin Asok <aswinasokofficial@gmail.com>
2025-08-31 13:46:29 +0530

    feat(appRouter):equiped-with-pageLoader

--- .claude/HomePage.tsx
+++ .claude/HomePage.tsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router";
 import styles from "./HomePage.module.scss";
+import { useTranslation } from "react-i18next";
 import { Header } from "@/widgets/Header";
 import { Footer } from "@/widgets/Footer";
 import { Spinner } from "@/shared/ui";
