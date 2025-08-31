7cf017ed7d8f4af11db697b31d1a42e190336967
Aswin Asok <aswinasokofficial@gmail.com>
2025-08-31 20:09:23 +0530

    feat(storyBook):theme-added

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,21 @@ import { ThemeDecorator } from "../src/shared/config/storybook";
 import "../src/app/styles/index.scss";
 
 const preview: Preview = {
+    globalTypes: {
+        theme: {
+            name: "Theme",
+            description: "App Theme",
+            toolbar: {
+                icon: "circleHollow",
+                items: [
+                    { value: "blue-theme", title: "Blue" },
+                    { value: "pink-theme", title: "Pink" },
+                ],
+                dynamicTitle: true,
+            },
+            defaultValue: "pink-theme",
+        },
+    },
     parameters: {
         controls: {
             matchers: {
